a22d6c93867e8b7031ef3247b165b76030a63408
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-17 14:10:21 -0500

    added models module

---
+++ mmic_autodock/models/output.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from mmelemental.models.app.docking import DockInput
+from mmelemental.models.util.output import ComputeOutput
+from pydantic import Field
+
+
+class AutoDockComputeOutput(ComputeOutput):
+    dockInput: DockInput = Field(..., description="Docking input model.")
+    scores: List[float] = Field(
+        None,
+        description="A metric for evaluating a particular pose. Length of scores must be equal to length of poses.",
+    )
+    poses: Optional[List[str]] = Field(
+        None,
+        description="List of file strings defining the conformation and orientation of the candidate ligand relative to the receptor.",
+    )
+    flexible: Optional[List[str]] = Field(
+        None,
+        description="List of file strings defining the conformation and orientation of the flexible side chains in the receptor "
+        "relative to the ligand.",
+    )
+    system: Optional[str] = Field(
+        None,
+        description="Input file string storing the ligand poses with (optionally) the flexible receptor side-chains.",
+    )
