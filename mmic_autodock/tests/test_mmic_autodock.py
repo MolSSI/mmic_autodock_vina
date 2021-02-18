5fad6a1b84a8ea93aad23ed20b225065594018d5
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-18 04:13:26 -0500

    removed whitespace

--- mmic_autodock/tests/test_mmic_autodock.py
+++ mmic_autodock/tests/test_mmic_autodock.py
@@ -7,6 +7,7 @@ import mmic_autodock
 import pytest
 import sys
 
+
 def test_mmic_autodock_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic_autodock" in sys.modules
