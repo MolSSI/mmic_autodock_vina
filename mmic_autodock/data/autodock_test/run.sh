56cde84da29737b04ac187ae88e6b657f2adae3b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-18 02:43:53 -0500

    added missing --flex arg

--- mmic_autodock/data/autodock_test/run.sh
+++ mmic_autodock/data/autodock_test/run.sh
@@ -16,6 +16,7 @@ rm input/ligand_out.pdbqt
 vina --center_x 11 --center_y 90.5 --center_z 57.5 \
      --size_x 22 --size_y 24 --size_z 28 \
      --receptor input/receptor_rigid.pdbqt \
+     --flex input/sidechain.pdbqt \
      --ligand input/ligand.pdbqt \
      --exhaustiveness 8 \
      --log results/flexible/vina.log
