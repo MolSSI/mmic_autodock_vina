f9bb4446fc97b19b3a493345bbd38b0d277b0a4b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-12 12:54:01 -0400

    merged test_sim.py into test_mmic_autodock.py

--- mmic_autodock_vina/tests/test_mmic_autodock.py
+++ mmic_autodock_vina/tests/test_mmic_autodock.py
@@ -4,6 +4,8 @@ Unit and regression test for the mmic_autodock_vina package.
 
 # Import package, test suite, and other packages as needed
 import mmic_autodock_vina
+from mmelemental.models.molecule import Molecule
+from mmic_docking.models.input import DockInput
 import pytest
 import sys
 
@@ -11,3 +13,36 @@ import sys
 def test_mmic_autodock_vina_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic_autodock_vina" in sys.modules
+
+def test_mmic_autodock_vina_run():
+    """ Test all the components via a docking compute. """
+    # Construct docking input
+    receptor = Molecule.from_file("mmic_autodock_vina/data/PHIPA_C2/PHIPA_C2_apo.pdb")
+    ligand = Molecule.from_data(
+        "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O", dtype="smiles"
+    )  # smiles code for ibuprofen
+
+    searchSpace = [-37.807, 5.045, -2.001, 30.131, -19.633, 37.987]
+
+    dockInput = DockInput(
+        molecule={"ligand": ligand, "receptor": receptor},
+        searchSpace=searchSpace,
+        searchSpace_units="angstrom",
+    )
+
+    # Import simulation component for autodock vina
+    from mmic_autodock_vina.components.autodock_component import AutoDockComponent
+
+    # Run autodock vina
+    dockOutput = AutoDockComponent.compute(dockInput)
+
+    # Extract output
+    scores, ligands, flex = (
+        dockOutput.scores,
+        dockOutput.poses.ligand,
+        dockOutput.poses.receptor,
+    )
+
+    assert len(scores) == len(ligands)
+    assert isinstance(scores, list)
+    # add more assertions here
